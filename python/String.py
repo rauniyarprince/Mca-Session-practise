2f374ac2df1d8f0f8179d26896b7b89a3214c6dd
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-09-22 17:14:38 +0530

    practise session

---
+++ python/String.py
@@ -0,0 +1,18 @@
+name = input("enter the name here: ")
+print("your name length is: ",len(name))
+print("\n")
+print("Displaying char using indexing:/n")
+adress = "Gorakhpur"
+print(adress[-1])
+print(adress[6])
+print("using slicing:/n")
+name = "prince rauniyar"
+print(name[0:3])
+print(name[:2])
+print(name[:-1])
+print(name[-1:-4])
+print("Using function\n")
+print(name.upper())
+print(name.lower())
+
+
